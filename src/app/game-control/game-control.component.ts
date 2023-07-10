a431cc9c9bbb868b4c940b2b86a9dc4197c75a28
Devashree Gosavi <35646522+devashreegosavi@users.noreply.github.com>
2023-07-10 11:49:18 +0530

    10 july 2023

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class GameControlComponent {
 
+    interval :any;
+    @Output() intervalFired = new EventEmitter<number>();
+    lastnumber=0;
+    onStartGame(){
+        this.interval=setInterval(()=>{
+          this.intervalFired.emit(this.lastnumber+1);
+          this.lastnumber++;
+        },1000)
+    }
+    onPauseGame(){
+      clearInterval(this.interval);
+    }
 }
